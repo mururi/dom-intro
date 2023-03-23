ac69a67595e80aaf4275eaf00e839725d6f069a1
Dennis Kiboi <mururi.kiboi@gmail.com>
2023-03-23 17:11:55 +0300

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let cards = document.querySelectorAll('.card');
+console.log(cards);
+
+// Get single elements from the DOM
+// document.querySelector('any-css-selector')
+// document.getElementById('id-name')
+
+//Get multiple elements from the DOM
+// document.querySelectorAll('any-css-selector')
+// document.getElementsByClassName('class-name')
